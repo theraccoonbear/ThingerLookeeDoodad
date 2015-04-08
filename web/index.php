b07aa5bbb0cf22e0e4fdbe91d0b7c595203b5ea9
dsmith <dsmith@shineunited.com>
2015-04-08 15:48:30 -0500

    back to working-ish

--- web/index.php
+++ web/index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-//ERROR_REPORTING(-1);
-ERROR_REPORTING(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE);
 
 function pr($obj) {
 	print '<pre>';
@@ -12,6 +8,12 @@ function pr($obj) {
 }
 
 require_once __DIR__.'/../vendor/autoload.php';
+
+use Silex\Application;
+use Silex\Provider;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
@@ -23,6 +25,7 @@ use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\SecurityServiceProvider;
 
 
+
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
 
 
@@ -31,6 +34,14 @@ use Doctrine\DBAL\Schema\Table;
 $app = new Silex\Application();
 
 $app['debug'] = true;
+
+if ($app['debug']) {
+	ini_set('display_errors',1);
+	ini_set('display_startup_errors',1);
+	//ERROR_REPORTING(-1);
+	ERROR_REPORTING(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE);
+}
+
 
 // register twig
 $app->register(new TwigServiceProvider());
@@ -41,6 +52,22 @@ $app['twig.loader.filesystem']->prependPath(__DIR__ . '/../tmpl/');
 
 $app->match('/', function ($stuff = false) use ($app) {
 	return $app['twig']->render('home.twig.html', array('copy' => 'AHA!'));
+});
+
+
+$app->match('/api/template/{name}', function($name) use ($app) {
+	$name = preg_replace('/[^A-Za-z0-9-]/', '', $name);
+	$tmpl_path = __DIR__ . '/../templates/' . $name . '.mst.html';
+	if (!file_exists($tmpl_path)) {
+		return new Response(
+			'MISSING TEMPLATE',
+			404,
+			array('Content-Type' => 'text/plain')
+		);
+	} else {
+		return file_get_contents($tmpl_path);
+	}
+
 });
 
 $app->run();
--- web/js/tld.js
+++ web/js/tld.js
@@ -1,48 +1,34 @@
-var TLD = (function() {
-	// THINGER
-	var thinger = function(o) {
-		var ctxt = this;
-		
-		var _data = {};
-		
-		this.options = o || {};
-		
-		
-	}; // thinger()
-	
-	var p_thinger = thinger.prototype;
-	
-	p_thinger.loadData = function(data) {
-		_data = data;
-	}; // t.loadData()
-	
-	p_thinger.dbg = function() {
-		console.log('_data', _data);
-	}; // t.dbg()
-	
-	p_thinger.bind = function() {
-		
-	}; // t.bind()
-	
-	// END THINGER
-	
-	// LOOKEE
-	var lookee = function() {
-		
-	}; // lookee()
-	
-	// END LOOKEE
-	
-	// DOODAD
-	var doodad = function() {
-		
-	}; // doodad()
-	
-	// END DOODAD
-	
-	return {
-		T: thinger,
-		L: lookee,
-		D: doodad
+var TLD = function (ready) {
+	TLD.ready = ready;
+
+	TLD.load = function(url, cb) {
+		$.get(url, {}, function(d, s, x) {
+			if (typeof cb === 'function') {
+				cb(d);
+			}
+		});
 	};
-})();
+	
+	TLD.drill = function(obj, bits, def) {
+		def = typeof def === 'undefined' ? '' : def;
+		bits = $.isArray(bits) ? bits : [bits];
+		return bits.length == 0 ? obj : (typeof obj[bits[0]] === 'undefined' ? def : drill(obj[bits[0]], bits.slice(1), def));
+	};
+	
+	require(["js/thinger"], function(t) {
+		require(["js/lookee"], function(l) {
+			require(["js/doodad"], function(t) {
+				if (typeof TLD.ready === 'function') {
+					TLD.ready();
+				} else {
+					console.log("No ready function defined.");
+				}
+			});		
+		});
+	});
+};
+
+
+
+
+
